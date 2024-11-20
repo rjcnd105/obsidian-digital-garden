7510e2cab412ceac86a3e2606877b749232a9a61
김회준 <rjcnd123@gmail.com>
2024-11-20 18:52:27 +0900

    ff

--- .eleventy.js
+++ .eleventy.js
@@ -559,14 +559,6 @@ module.exports = function (eleventyConfig) {
     },
   });
 
-  // shiki 설정을 나중에 적용
-  import('@shikijs/markdown-it').then(async ({ default: shiki }) => {
-    await markdownLib.use(await shiki({
-      theme: 'catppuccin-mocha', // 또는 원하는 테마
-      langs: ['javascript', 'typescript', 'shellscript', 'latex', 'templ', 'typespec', 'typst','yaml', 'postcss','regexp','sql','terraform', 'haskell', "bash", 'nix', 'elixir', 'erlang', 'gleam', 'rust', 'markdown', 'css', 'sass', 'html', 'toml', 'docker', 'dotenv', 'dart', 'ini', 'json', 'jsonc', 'make', 'mdx', 'mermaid', 'ocaml', 'jsx', 'tsx', 'xml'] // 지원하고 싶은 언어들
-    }));
-  });
-
   userEleventySetup(eleventyConfig);
 
   return {
--- src/helpers/userSetup.js
+++ src/helpers/userSetup.js
@@ -1,6 +1,15 @@
 function userMarkdownSetup(md) {
   // The md parameter stands for the markdown-it instance used throughout the site generator.
   // Feel free to add any plugin you want here instead of /.eleventy.js
+
+
+    // shiki 설정을 나중에 적용
+    import('@shikijs/markdown-it').then(async ({ default: shiki }) => {
+        await md.use(await shiki({
+            theme: 'catppuccin-mocha', // 또는 원하는 테마
+            langs: ['javascript', 'typescript', 'shellscript', 'latex', 'templ', 'typespec', 'typst','yaml', 'postcss','regexp','sql','terraform', 'haskell', "bash", 'nix', 'elixir', 'erlang', 'gleam', 'rust', 'markdown', 'css', 'sass', 'html', 'toml', 'docker', 'dotenv', 'dart', 'ini', 'json', 'jsonc', 'make', 'mdx', 'mermaid', 'ocaml', 'jsx', 'tsx', 'xml'] // 지원하고 싶은 언어들
+        }));
+    });
 }
 function userEleventySetup(eleventyConfig) {
   // The eleventyConfig parameter stands for the the config instantiated in /.eleventy.js.
